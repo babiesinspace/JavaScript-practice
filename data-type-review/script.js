f80ea87fde533e1cdd28d66bcceebd60cd660726
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-06 14:06:50 -0500

    create index and script files, copy/paste instructions

---
+++ data-type-review/script.js
@@ -0,0 +1,69 @@
+// Javascript Review: Data Types | Variables | Logic | Loops
+
+
+// 1. Intro to Javascript
+
+// Create a script using JS that will alert a user with “Welcome to NYCDA, This is your first Javascript!” as well as log it into your developer console. 
+
+
+// 2. Variables and Math
+
+// Let’s do some basic math and variable assignments. 
+// Create a variable called mathOne and give it a value of 20 , create another one called mathTwo and give it a value of 3.  
+// Create a new variable called mathSum that will be the sum of the first variables. 
+// Create another variabled called mathProduct and this will be the product of the first two variables.
+// Print mathSum and mathProduct to your developer console.
+
+
+// 3. Mixing Data Types
+
+// Now lets create a string variable called test and give it a value of your name. 
+// Create another variable called myAge and give it a value of your age. 
+// Add those two together and see what happens! Do you get a string or a number back? How about if we add a number and a boolean?
+
+
+// 4. Arrays & Multidimensional Arrays 
+
+// Create an array with the name of 5 of your classmates (If you don’t remember than ask!). 
+// Access the classmate in the 4th position of your array. 
+// Add a new classmate into your array!
+// The classmate in the 2nd position of your array has gone missing. Lets remove them from the array. 
+// Remove the last element of our array without specifying its index. 
+// Given the following multidimensional array: 
+// Access the array and return the element that contains ‘beans’
+
+
+// 5. Object Literals
+
+// Create a object literal with your top 3 movies using the keys : topMovieOne , topMovieTwo , topMovieThree. 
+// Assign some movie values to your keys!
+// Access your object literal and print out in the console the following string “ My favorite movies are {topMovieOne} and {topMovieTwo}, but I really enjoy {topMovieThree}”. 
+
+
+
+
+// 6. Object Literals & Changing Values
+// Create an object with name and eyeColor properties. Change the eye color to 'brown'. Now add a hairColor property and change it to 'blonde'"
+
+
+// 7. Logic
+// Write a conditional that checks if a variable is less than 10. If it is, alert the user that their variable is less than 10. If it is not, let the user know what the variable was and that it was greater than 10.
+// Write a program that evaluates two things: 
+// temperature and the status of an air conditioner (on or off). 
+// If the temperature is warmer than 80, and the air conditioner is off, log "turn the ac on!".
+//  If the temperature is warmer than 80 and the ac is on, log "this thing is broken!". 
+// If the temperature is colder than 60 and the air conditioner is on, log "brrr, turn this thing off".
+// If the temperature is cooler than 80, and the ac is off, log "not worth the electricity. leave it off." 
+
+
+// 8. Logic & Strict Testing
+//  Write a conditional that checks if our variable is equal to the number 45.
+
+
+
+// 9. Loops ( For )
+// Create a for loop that will print out asterisks starting with 10 of them down to one. 
+// Example : 
+// ********** (10)
+// *********(9)
+// ********(8) 
