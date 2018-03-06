76278d1e074019186bc6d6743378175d53c92c92
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-06 16:41:15 -0500

    Counting Crows

--- javascript-challenges/script.js
+++ javascript-challenges/script.js
@@ -11,3 +11,12 @@ for (i = 0; i < prices.length; i++) {
   sum = sum + prices[i];
 }
 
+// 2 - 99 Crows on the Wall
+// Write a program in a new Repl.it that outputs all 100 lines of the classic song: 99 Crows on the Wall. 
+
+for (i = 99; i > 1; i--) {
+  console.log(`${i} crows on the wall. ${i} crows.\n1 fell down and became a wight.\n${i-1} crows on the wall`)
+}
+
+console.log("1 crow on the wall. 1 single crow.\nIt fell down and became a wight.\nThere's no one left to defend Westeros now.")
+
