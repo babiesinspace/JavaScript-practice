1e3f036187ee36019fdac2c66973a4447da6f95e
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-06 16:35:12 -0500

    write sum function

--- javascript-challenges/script.js
+++ javascript-challenges/script.js
@@ -1,1 +1,13 @@
-alert("We're in business!")
+// alert("We're in business!")
+
+// 1 - A running total.
+// Using a for loop, compute the sum of the numbers in the array named prices; storing the result in the sum variable.
+
+var prices = [33,26,99,120,12,45];
+
+var sum = 0;
+
+for (i = 0; i < prices.length; i++) {
+  sum = sum + prices[i];
+}
+
