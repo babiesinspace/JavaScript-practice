1e8f9740ec65a9bf4dec5ea1184dd099f4e77d15
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-06 09:58:36 -0500

    promts and alerts

---
+++ stalker/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you?");
+
+console.log("Hello " + firstName + " " + lastName + ", it's nice to meet you.")
+console.log("You don't look " + age + "...")
