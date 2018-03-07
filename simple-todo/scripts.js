a8c98b3c1003795b8cda07600742b3331a3025e9
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-07 13:21:25 -0500

    set interval timer to slow prompts, create loop to ask user for input until they want to quit

--- simple-todo/scripts.js
+++ simple-todo/scripts.js
@@ -1,1 +1,18 @@
-alert("connected!")
+// let input = prompt("What would you like to do?");
+
+// let toDoList = [];
+
+window.setTimeout(function() {
+  let input = undefined;
+
+  let toDoList = [];
+
+  while (input !== 'quit'){
+
+  input = prompt("What would you like to do?");
+  
+  }
+
+  alert("Great! See you next time!")
+
+}, 500);
