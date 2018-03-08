4be0baa5307ea36fb6cdf11227201a29ebb077c4
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-08 16:29:41 -0500

    flight checker workshop

--- algos/script.js
+++ algos/script.js
@@ -15,3 +15,37 @@ function isPrime(number) {
   return true;
 }
 
+// Users on longer flights like to start a second movie right when their first one ends, but they complain that the plane usually lands before they can see the ending. 
+// So you're building a feature for choosing two movies whose total runtimes will equal the exact flight length.
+
+// Write a program that takes an integer flightLength (in minutes) and an array of integers movieLengths (in minutes) and returns a boolean indicating whether there are two numbers 
+// in movieLengths whose sum equals flightLength.
+
+// When building your method:
+
+// 1. Assume your users will watch exactly two movies
+// 2. Don't make your users watch the same movie twice
+
+let movieLengths = [70,120,230,93,89,45,168,345]
+let flightLength = 165
+
+//input array and flight times
+function movieFlightChecker(movieLengths, flightLength) {
+  //check if element from array is longer than flight
+  let perfectMatch = false;
+  movieLengths.forEach(function(movie){
+    if (movie < flightLength) {
+    //check if element can sum with any other to equal flight time
+    //if no, check next element with each proceeding element
+    for ((i = movieLengths.indexOf(movie) + 1); i < (movieLengths.length - 1); i++) {
+      //if yes, return true
+      if (movie + movieLengths[i] === flightLength) {
+        perfectMatch = true;
+      }
+    }      
+    }
+  });
+  return perfectMatch;
+}
+movieFlightChecker(movieLengths,flightLength);
+//returns true
