b61e99a89904dcc4d9288bdfee46330a3f68f8a4
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-12 17:15:33 -0400

    check password on click

--- DOMinate/script.js
+++ DOMinate/script.js
@@ -1,5 +1,19 @@
-let newH1 = document.getElementsByTagName('H1')[0]
+//Change h1 to whatever key is pressed
 
-document.addEventListener("keypress", function (event) {
-  newH1.innerText = event.key
+// let newH1 = document.getElementsByTagName('H1')[0]
+
+// document.addEventListener("keypress", function (event) {
+//   newH1.innerText = event.key
+// })
+
+let button = document.getElementById("submit-button")
+
+let password = document.getElementById("password")
+
+let username = document.getElementById("username")
+
+button.addEventListener("click", function() {
+  if (password.value != "12345678") {
+    alert("incorrect")
+  } 
 })
