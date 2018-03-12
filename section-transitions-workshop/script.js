2e55f8f8919437618800c360aaeec0504396ce20
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-12 19:14:04 -0400

    Add code to also fade sections out

--- section-transitions-workshop/script.js
+++ section-transitions-workshop/script.js
@@ -1,5 +1,6 @@
 let scrollPos = window.scrollY;
 
+let section1 = document.getElementById('section1');
 let section2 = document.getElementById('section2');
 let section3 = document.getElementById('section3');
 let section4 = document.getElementById('section4');
@@ -11,16 +12,29 @@ function reveal(section) {
   }
 };
 
+function fadeOut(section) {
+  for (i = 0; i < section.children.length; i++) {
+    section.children[i].classList.remove("show"); 
+  }
+};
+
 window.addEventListener('scroll', function(){ 
     scrollPos = window.scrollY;
-    if (scrollPos > 750) {
-      console.log(section2)
-      reveal(section2)
+    if (scrollPos < 750) {
+      reveal(section1);
+      fadeOut(section2);
+    }    
+    if (scrollPos > 750 && scrollPos < 1250) {
+      fadeOut(section1)
+      reveal(section2);
+      fadeOut(section3);
     }
-    if (scrollPos > 1550) {
-      reveal(section3)
+    if (scrollPos > 1550 && scrollPos < 2050) {
+      reveal(section3);
+      fadeOut(section4);
     }
     if (scrollPos > 2350) {
-      reveal(section4)
-    }
+      reveal(section4);
+      fadeOut(section3);
+    } 
 });
