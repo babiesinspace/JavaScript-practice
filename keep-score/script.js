124c8c4882e2e95e9883594b9d71fadc78764797
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-13 12:02:50 -0400

    reset score and enable buttons on player reset

--- keep-score/script.js
+++ keep-score/script.js
@@ -1,18 +1,16 @@
 let p1button = document.getElementById('player1')
-
 let p2button = document.getElementById('player2')
+let reset = document.getElementById('reset')
 
 let p1score = document.querySelector('#p1score')
-
 let p2score = document.querySelector('#p2score')
 
 let setFirstTo = document.getElementById('firstTo')
-
 let playingTo = document.getElementById('playingTo')
 
 function foundWinner(){
-  p1button.disabled = true;
-  p2button.disabled = true;
+  p1button.disabled = true
+  p2button.disabled = true
 }
 
 function toNumber(score){
@@ -27,6 +25,23 @@ function scoreUp(score){
   let num = toNumber(score)
   return addPoint(num)
 }
+
+function enableButtons(){
+  p1button.disabled = false
+  p2button.disabled = false
+}
+
+function resetScore(){
+  p1score.innerText = 0;
+  p1score.style.color = 'black'
+  p2score.innerText = 0;
+  p2score.style.color = 'black'
+}
+
+reset.addEventListener("click", function(){
+  enableButtons()
+  resetScore()
+})
 
 p1button.addEventListener("click", function() {
   let newScore = scoreUp(p1score.innerText)
