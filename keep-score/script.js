4f8e411f150d19e98c4e597984cbd03a4f06dbfd
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-13 11:35:47 -0400

    Change player score based on button click

--- keep-score/script.js
+++ keep-score/script.js
@@ -2,10 +2,29 @@ let p1button = document.getElementById('player1')
 
 let p2button = document.getElementById('player2')
 
+let p1score = document.querySelector('#p1score')
+
+let p2score = document.querySelector('#p2score')
+
+function toNumber(score){
+  return Number(score);
+}
+
+function addPoint(scoreAsNumber) {
+  return scoreAsNumber += 1;
+}
+
+function scoreUp(score){
+  let num = toNumber(score)
+  return addPoint(num)
+}
+
 p1button.addEventListener("click", function () {
-  alert("clicked!")
+  let newScore = scoreUp(p1score.innerText)
+  p1score.innerText = newScore;
 })
 
 p2button.addEventListener("click", function () {
-  alert("clicked!")
+  let newScore = scoreUp(p2score.innerText)
+  p2score.innerText = newScore;
 })
