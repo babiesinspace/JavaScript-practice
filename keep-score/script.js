217319ea00011c89c1838a4621e3915e7ffb2e3d
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-03-13 11:00:26 -0400

    add event listeners on player buttons

---
+++ keep-score/script.js
@@ -0,0 +1,11 @@
+let p1button = document.getElementById('player1')
+
+let p2button = document.getElementById('player2')
+
+p1button.addEventListener("click", function () {
+  alert("clicked!")
+})
+
+p2button.addEventListener("click", function () {
+  alert("clicked!")
+})
